f15d102d96104d3ded36a26a7ccff2f89579170b
TH3-G3NTL3M3N <ruellands@gmail.com>
2025-06-03 16:46:17 -0400

    Fix SelectField story for Storybook v8 API

--- app/src/components/Fields/SelectField.stories.tsx
+++ app/src/components/Fields/SelectField.stories.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import SelectField from './SelectField';
 
-export default {
+const meta: Meta<typeof SelectField> = {
   title: 'SelectField',
   component: SelectField,
   argTypes: {
@@ -14,30 +14,33 @@ export default {
   parameters: {
     layout: 'centered',
   },
-} as Meta<typeof SelectField>;
-
-const Template: ComponentStory<typeof SelectField> = (args) => {
-  const [value, setValue] = useState<string | undefined>(undefined);
-  return (
-    <div className="w-full p-8">
-      <SelectField
-        id="select-field"
-        {...args}
-        value={value as string}
-        options={['Value 1', 'Value 2', 'Value 3']}
-        onChange={setValue}
-        optionToLabel={(option: string) => option}
-        optionToKey={(option: string) => option}
-      />
-    </div>
-  );
 };
 
-export const Default = Template.bind({});
-Default.args = {
-  placeholder: 'Select an option',
-  disabled: false,
-  error: '',
-  tip: 'This is a tip',
-  label: 'Options',
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args: React.ComponentProps<typeof SelectField>) => {
+    const [value, setValue] = useState<string | undefined>(undefined);
+    return (
+      <div className="w-full p-8">
+        <SelectField
+          id="select-field"
+          {...args}
+          value={value as string}
+          options={['Value 1', 'Value 2', 'Value 3']}
+          onChange={setValue}
+          optionToLabel={(option: string) => option}
+          optionToKey={(option: string) => option}
+        />
+      </div>
+    );
+  },
+  args: {
+    placeholder: 'Select an option',
+    disabled: false,
+    error: '',
+    tip: 'This is a tip',
+    label: 'Options',
+  },
 };
